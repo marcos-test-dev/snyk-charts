ed2f57e861a8cbb69c7d8c76e7e9d4c5a853109a
Marcos Bergami <marcos.bergami@snyk.io>
2022-06-28 23:18:50 -0500

    feat: trends development

--- snyk-charts.py
+++ snyk-charts.py
@@ -2,18 +2,43 @@ import os
 import requests
 import time
 import json
+import pandas as pd
 from getpass import getpass
 import plotly.graph_objects as go
+import pyfiglet
+from simple_term_menu import TerminalMenu
+from rich import print as rprint
 
 
-if not os.path.exists("images"):
-    os.mkdir("images")
 
 def main():
-    orgId = input("Please input your organization's ID:")
-    apiToken = getpass(prompt='Please input your token:')
+    if not os.path.exists("images"):
+        os.mkdir("images")
+    
+    with open("parameters.txt") as f:
+        data = {}
+        for line in f:
+            key, value = line.strip().split("=")
+            data[key] = value
+    f.close()
 
-    endpoint = "https://snyk.io/api/v1/reporting/counts/issues?from=2022-05-01&to=2022-06-27&groupBy=severity"
+    orgId = data['ORGID']
+    apiToken = data['TOKEN']
+    startDate = data['START_DATE']
+    endDate = data['END_DATE']
+
+    title = pyfiglet.figlet_format("Snyk Charts", font="slant")
+    rprint("[bold magenta]" + title)
+    rprint("[cyan]GENERATE INTERACTIVE CHARTS DERIVED FROM THE SNYK API[/cyan] \n \n")
+
+    print("Loading   ", end="")
+
+    selection = input("Which endpoint to follow ?")
+
+    if selection == "old":
+        endpoint = "https://snyk.io/api/v1/reporting/counts/issues?from=2022-05-01&to=2022-06-27&groupBy=severity"
+    else:
+        endpoint = "https://snyk.io/api/v1/reporting/issues/?from=2022-05-01&to=2022-06-27&page=1&perPage=100&sortBy=issueTitle&order=asc&groupBy=issue"
 
     # endpoint = "https://snyk.io/api/v1/reporting/counts/issues?from=" + 2017-07-01 + "&to=" + 2017-07-03 + "&groupBy=severity"
 
@@ -21,6 +46,8 @@ def main():
     'Content-Type': 'application/json',
     'Authorization': 'token ' + apiToken
     }
+
+    print("\rLoading.  ", end="")
 
     values = json.dumps({
         "filters": {
@@ -32,9 +59,9 @@ def main():
             "low"
             ],
             "types": [
-            "vuln",
-            "license",
-            "configuration"
+            "vuln"
+            # "license",
+            # "configuration"
             ],
             "languages": [
             "node",
@@ -70,48 +97,75 @@ def main():
         }
     }, indent=4)
 
+    print("\rLoading.. \n", end="")
 
     request = requests.request("POST", endpoint, headers=headers, data=values)
     response = request.json()
 
-    time_period = []
+    if selection == "old":
+        time_period = []
 
-    low_count = []
-    medium_count = [] 
-    high_count = []
-    critical_count = []
+        low_count = []
+        medium_count = [] 
+        high_count = []
+        critical_count = []
 
-    while response['results']:
-        result = response['results'].pop()
-        date = result['day']
-        time_period.append(date)
-        severity = result['severity']
-        critical_count.append(severity['critical'])
-        high_count.append(severity['high'])
-        medium_count.append(severity['medium'])
-        low_count.append(severity['low'])
+        print("\rLoading...", end="")
 
-    fig = go.Figure()
-    # Create and style traces
-    fig.add_trace(go.Scatter(x=time_period, y=low_count, name='Low',
-                             line=dict(color='gray', width=4)))
-    fig.add_trace(go.Scatter(x=time_period, y=medium_count, name = 'Medium',
-                             line=dict(color='burlywood', width=4)))
-    fig.add_trace(go.Scatter(x=time_period, y=high_count, name = 'High',
-                             line=dict(color='coral', width=4)))
-    fig.add_trace(go.Scatter(x=time_period, y=critical_count, name = 'Critical',
-                             line=dict(color='crimson', width=4)))
+        while response['results']:
+            result = response['results'].pop()
+            date = result['day']
+            time_period.append(date)
+            severity = result['severity']
+            critical_count.append(severity['critical'])
+            high_count.append(severity['high'])
+            medium_count.append(severity['medium'])
+            low_count.append(severity['low'])
 
-    # Edit the layout
-    fig.update_layout(title='Issues over time',
-                       xaxis_title='Month',
-                       yaxis_title='Issues')
+        print("Chart coming out of the oven!")
+        fig = go.Figure()
+        # Create and style traces
+        fig.add_trace(go.Scatter(x=time_period, y=low_count, name='Low',
+                                line=dict(color='gray', width=4)))
+        fig.add_trace(go.Scatter(x=time_period, y=medium_count, name = 'Medium',
+                                line=dict(color='burlywood', width=4)))
+        fig.add_trace(go.Scatter(x=time_period, y=high_count, name = 'High',
+                                line=dict(color='coral', width=4)))
+        fig.add_trace(go.Scatter(x=time_period, y=critical_count, name = 'Critical',
+                                line=dict(color='crimson', width=4)))
 
-    ts = str(time.time())
-    ts = ts.replace(".","")
+        # Edit the layout
+        fig.update_layout(title='Issues over time',
+                        xaxis_title='Month',
+                        yaxis_title='Issues')
 
-    fig.write_image("images/" + ts + "chart.png")
-    fig.show()
+        ts = str(time.time())
+        ts = ts.replace(".","")
+
+        fig.write_image("images/" + ts + "chart.png")
+        fig.show()
+    else:
+        issueList = []
+
+        while response['results']:
+            result = response['results'].pop()
+            issue = result['issue']
+            issueTitle = issue['title']
+            issueList.append(issueTitle)
+        
+        df = pd.DataFrame(issueList, columns=["Issues"])
+
+        issuesLabel = df["Issues"].value_counts().keys().tolist()
+        issuesCount = df["Issues"].value_counts().tolist()
+
+        fig = go.Figure([go.Bar(x=issuesLabel, y=issuesCount)])
+        
+        fig.update_layout(title="Trending issues " + startDate + " - " + endDate)
+
+        fig.show()
+
+        
+        
 
 if __name__ == "__main__":
   main()
